3bf2672c15f64a919bac9c829b999a230803fbf9
pirschous <110510027+pirschous@users.noreply.github.com>
2023-01-30 18:39:25 -0800

    Commiting to main branch
    JavaScript code

---
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,14 @@
+A = "This is my JavaScript!"//Defining a variable and giving it a string value
+
+window.alert(A);//Putting the value in the window alert box
+
+document.write("Her sister told her, \"Stop or I am telling mom!\"<br>")//Putting the value of the string into a function
+
+document.write("\"Be who you are and say what you feel,"//Putting the value of the string into a function
++ " because those who mind don\'t matter and those who matter don\'t mind.\""
++ "-Dr. Seuss<br>");
+
+var Family = "The Pirsch's", Dad = "James", Mom = "Stephanie", Daughter1 = "Kennedy", Daughter2 = "Peyton";
+document.write(Dad)//Putting the value of the string into a function
+
+document.write(3 + 3)//Putting the value of the string into a function
