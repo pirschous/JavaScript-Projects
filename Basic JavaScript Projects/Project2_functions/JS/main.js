22fdc7601b8219bedae3e2fb8651d103e851e35a
pirschous <110510027+pirschous@users.noreply.github.com>
2023-01-31 20:24:02 -0800

    Update main.js

--- Basic JavaScript Projects/Project2_functions/JS/main.js
+++ Basic JavaScript Projects/Project2_functions/JS/main.js
@@ -1,8 +1,8 @@
-function displayDate() {
+function displayDate() {//This is a function that is calling 2 elements. 
     document.getElementById("demo").innerHTML = Date();
   }
 
-  function myFunction() {
+  function myFunction() {//Function element that calls the variables sentence. 
     var sentence = "I am learning";
     sentence += " a lot from this book!";
     document.getElementById("Concatenate").innerHTML = sentence;
