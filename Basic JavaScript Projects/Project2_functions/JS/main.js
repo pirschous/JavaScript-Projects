4eb5fbd23da20675b7452c9743fb28d02ce79e9a
pirschous <110510027+pirschous@users.noreply.github.com>
2023-01-31 20:11:31 -0800

    JavaScript first code

---
+++ Basic JavaScript Projects/Project2_functions/JS/main.js
@@ -0,0 +1,9 @@
+function displayDate() {
+    document.getElementById("demo").innerHTML = Date();
+  }
+
+  function myFunction() {
+    var sentence = "I am learning";
+    sentence += " a lot from this book!";
+    document.getElementById("Concatenate").innerHTML = sentence;
+  }
