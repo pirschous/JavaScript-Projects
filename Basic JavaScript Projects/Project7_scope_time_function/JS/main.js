20a66df283dced9feec5ee503525251e31c4185f
pirschous <110510027+pirschous@users.noreply.github.com>
2023-02-08 19:35:57 -0800

    Time project

--- Basic JavaScript Projects/Project7_scope_time_function/JS/main.js
+++ Basic JavaScript Projects/Project7_scope_time_function/JS/main.js
@@ -15,7 +15,49 @@ function Add_numbers_1() {
     document.write(20 + X + "<br>");
 }
 function Add_numbers_2() {
-    document.write(X + 100);
+    document.write(X + 100);    
 }
 Add_numbers_1();
 Add_numbers_2();
+
+function get_Date() {
+    if (new Date().getHours() > 18) {
+        document.getElementById("Greeting").innerHTML = "How are you today?";
+    }
+}
+
+document.write("<br>");
+
+if (4 < 8) {
+    document.write("I am finally starting to grasp this whole Javascript thing, very slowly!")
+}
+
+document.write("<br>");
+
+function Money_Function() {
+    Money = document.getElementById("Money").value;
+    if (Money >= 20) {
+        Buy = "You have enough money to buy this watch.";
+    }
+    else {
+        Buy = "You don't have enough money to buy this watch.";
+    }
+    document.getElementById("How_much_money?").innerHTML = Buy;
+}
+
+document.write("<br>");
+
+function Time_function() {
+    var Time = new Date().getHours();
+    var Reply;
+    if (Time < 12 == Time > 0) {
+        Reply = "It is morning time!";
+    }
+    else if (Time >= 12 == Time < 18) {
+        Reply = "It is afternoon.";
+    }
+    else {
+        Reply = "It is evening time.";
+    }
+    document.getElementById("Time_of_day").innerHTML = Reply;
+}
